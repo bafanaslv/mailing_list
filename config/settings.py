409ab65263fc44b885c75fad5f6bb427b5565839
Вячеслав Лукин <bafana.slv@gmail.com>
2024-08-01 06:37:19 +0300

    Реализация планировщика

--- config/settings.py
+++ config/settings.py
@@ -87,13 +87,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'static',
+# )
 
-# STATIC_URL = '/static/'
-# STATIC_PATH = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
