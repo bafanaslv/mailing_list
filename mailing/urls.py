3f0f9255ed789c5002484f49493d7d1ccb3fea2f
Вячеслав Лукин <bafana.slv@gmail.com>
2024-08-08 06:45:40 +0300

    Доработка проекта. Настройка приложения Users.

--- mailing/urls.py
+++ mailing/urls.py
@@ -5,13 +5,12 @@ from mailing import views
 app_name = MailingConfig.name
 
 urlpatterns = [
-    path('', views.MainView.as_view(), name='main'),
+    path('', views.ClientListView.as_view(), name='client_list'),
     path('mailing/', views.MailingListView.as_view(), name='list'),
     path('create/', views.MailingCreateView.as_view(), name='create'),
     path('<int:pk>/detail/', views.MailingDetailView.as_view(), name='detail'),
     path('<int:pk>/update/', views.MailingUpdateView.as_view(), name='update'),
     path('<int:pk>/delete/', views.MailingDeleteView.as_view(), name='confirm_delete'),
-    path('client/', views.ClientListView.as_view(), name='client_list'),
     path('client/create/', views.ClientCreateView.as_view(), name='client_form'),
     path('<int:pk>/client_update/', views.ClientUpdateView.as_view(), name='client_form'),
     path('<int:pk>/client_delete/', views.ClientDeleteView.as_view(), name='client_confirm_delete'),
